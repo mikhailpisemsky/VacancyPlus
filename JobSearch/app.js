3ae5fd795d5306630e958a1e28b40e0bab34b461
mikhailpisemsky <pisemskijmihail@gmail.com>
2025-04-23 05:40:26 +0500

    Connect to the database

--- JobSearch/app.js
+++ JobSearch/app.js
@@ -1,1 +1,21 @@
-console.log("Hello World!")
+const express = require('express');
+const config = require('config');
+const pool = require('./config/db');
+
+const app = express();
+
+const PORT = config.get('port') || 5000
+
+async function start() {
+    try {
+        pool.query('SELECT NOW()')
+        app.listen(PORT, () => {
+            console.log(`App has been started on port ${PORT}...`)
+        })
+    } catch(e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+}
+
+start()
