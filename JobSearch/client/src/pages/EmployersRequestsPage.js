30792c13a441524bfbdf0c71902f1bb82c4c9931
mikhailpisemsky <pisemskijmihail@gmail.com>
2025-09-12 23:18:16 +0500

    удалил лишние файлы

--- JobSearch/client/src/pages/EmployersRequestsPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const EmployersRequestsPage = () => {
-    return (
-        <div>
-            <h1>Заявки для просмотра работодателем</h1>
-        </div>
-    )
-}
--- JobSearch/client/src/pages/StudentsRequestsPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const StudentsRequestsPage = () => {
-    return (
-        <div>
-            <h1>Заявки студента</h1>
-        </div>
-    )
-}
